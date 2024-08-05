651f439ed062d0001efb071279bad8802e43360a
elodiegermani1 <elodie.germani@irisa.fr>
2024-08-05 14:39:21 +0200

    Run feature visualization

--- run_feature-visualization.py
+++ run_feature-visualization.py
@@ -19,10 +19,10 @@ def main(config):
 if __name__ == '__main__':
     
     config = {
-        data_dir : './data',
-        dataset : 'dataset_rh_4classes-jeanzay',
-        mode : 'test',
-        labels : 'pipelines'
+        'data_dir' : './data',
+        'dataset' : 'dataset_rh_4classes-jeanzay',
+        'mode' : 'test',
+        'labels' : 'pipelines'
 
     }
 
