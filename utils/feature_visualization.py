6e4d563bd61b6a5a26bb6ff94c16208dc03c975d
elodiegermani1 <elodie.germani@irisa.fr>
2024-08-05 15:20:16 +0200

    Run feature visualization

--- utils/feature_visualization.py
+++ utils/feature_visualization.py
@@ -85,7 +85,7 @@ def visualize_features(parameters_file, dataset, classe, print_title=True):
 		a = fig.add_subplot(1,5, i+1)
 		mean_img = np.mean(all_img[i], axis=0)
 
-		imgplot = plt.imshow(mean_img, cmap = nilearn_cmaps['cold_hot'], vmin=-1, vmax=1)
+		imgplot = plt.imshow(mean_img, cmap = nilearn_cmaps['cold_hot'], vmin=-5, vmax=5)
 		#plt.colorbar()
 		a.axis("off")
 		if print_title:
