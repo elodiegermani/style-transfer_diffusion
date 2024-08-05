466851beaadb06776c99d75a336231e2194b612e
elodiegermani1 <elodie.germani@irisa.fr>
2024-08-05 14:48:13 +0200

    Run feature visualization

--- run_feature-visualization.py
+++ run_feature-visualization.py
@@ -1,30 +1,29 @@
 from utils import feature_visualization
 from utils.datasets import ClassifDataset
 
-def main(config):
+def main(data_dir, mode, dataset, labels, param_file):
 
-    dataset_file = f'{config['data_dir']}/{config['mode']}-{config['dataset']}.csv'
+    dataset_file = f'{data_dir}/{mode}-{dataset}.csv'
 
     dataset = ClassifDataset(
         dataset_file, 
-        config['labels'])
+        labels)
 
     for i, classes in enumerate(dataset.label_list):
         print(classes)
         p=True
-        feature_visualization.visualize_features(config['param_file'], dataset, classe=classes, 
-                                                 classe_name=dataset.label_list[i], types='right-hand', print_title=p)
+        feature_visualization.visualize_features(param_file, dataset, classe=classes, 
+                                                 classe_name=dataset.label_list[i], 
+                                                 types='right-hand', print_title=p)
 
 
 if __name__ == '__main__':
     
-    config = {
-        'data_dir' : './data',
-        'dataset' : 'dataset_rh_4classes-jeanzay',
-        'mode' : 'test',
-        'labels' : 'pipelines',
-        'param_file' : './results/models/classifier_b-64_lr-1e-04_epochs_140.pth'
 
-    }
+    data_dir = './data',
+    dataset = 'dataset_rh_4classes-jeanzay',
+    mode = 'test',
+    labels = 'pipelines',
+    param_file = './results/models/classifier_b-64_lr-1e-04_epochs_140.pth'
 
     main(config)
